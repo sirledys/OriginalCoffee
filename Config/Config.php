158bfbcb42d9cabfe4422ef5d9d156cceed2f216
sirledys <sirledys.rangel@gmail.com>
2021-12-19 12:08:08 -0500

    originalcoffee

---
+++ Config/Config.php
@@ -0,0 +1,103 @@
+<?php 
+	const BASE_URL = "http://localhost/originalcoffee";
+	//const BASE_URL = "https://abelosh.com/tiendavirtual";
+
+	//Zona horaria
+	date_default_timezone_set('America/Guatemala');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "db_tiendavirtual2";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
+
+	//Para envío de correo
+	const ENVIRONMENT = 1; // Local: 0, Produccón: 1;
+
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "$";
+	const CURRENCY = "Pesos";
+
+	//Api PayPal
+	//SANDBOX PAYPAL
+	const URLPAYPAL = "https://api-m.sandbox.paypal.com";
+	const IDCLIENTE = "";
+	const SECRET = "";
+	//LIVE PAYPAL
+	//const URLPAYPAL = "https://api-m.paypal.com";
+	//const IDCLIENTE = "";
+	//const SECRET = "";
+
+	//Datos envio de correo
+	const NOMBRE_REMITENTE = "Original Coffe";
+	const EMAIL_REMITENTE = "danii.20151230@gmail.com";
+	const NOMBRE_EMPESA = "Original Coffe";
+	const WEB_EMPRESA = "www.original_coffe.com";
+
+	const DESCRIPCION = "La mejor tienda en línea de cafe";
+	const SHAREDHASH = "original_coffe";
+
+	//Datos Empresa
+	const DIRECCION = "Santa Marta";
+	const TELEMPRESA = "+(065) 42000000";
+	const WHATSAPP = "+(57) 300489745";
+	const EMAIL_EMPRESA = "info@originalcoffee.com";
+	const EMAIL_PEDIDOS = "info@originalcoffee.com"; 
+	const EMAIL_SUSCRIPCION = "info@originalcoffee.com";
+	const EMAIL_CONTACTO = "info@originalcoffee.com";
+
+	const CAT_SLIDER = "1,2,3";
+	const CAT_BANNER = "4,5,6";
+	const CAT_FOOTER = "1,2,3,4,5";
+
+	//Datos para Encriptar / Desencriptar
+	const KEY = 'abelosh';
+	const METHODENCRIPT = "AES-128-ECB";
+
+	//Envío
+	const COSTOENVIO = 5;
+
+	//Módulos
+	const MDASHBOARD = 1;
+	const MUSUARIOS = 2;
+	const MCLIENTES = 3;
+	const MPRODUCTOS = 4;
+	const MPEDIDOS = 5;
+	const MCATEGORIAS = 6;
+	const MSUSCRIPTORES = 7;
+	const MDCONTACTOS = 8;
+	const MDPAGINAS = 9;
+
+	//Páginas
+	const PINICIO = 1;
+	const PTIENDA = 2;
+	const PCARRITO = 3;
+	const PNOSOTROS = 4;
+	const PCONTACTO = 5;
+	const PPREGUNTAS = 6;
+	const PTERMINOS = 7;
+	const PSUCURSALES = 8;
+	const PERROR = 9;
+
+	//Roles
+	const RADMINISTRADOR = 1;
+	const RSUPERVISOR = 2;
+	const RCLIENTES = 3;
+
+	const STATUS = array('Completo','Aprobado','Cancelado','Reembolsado','Pendiente','Entregado');
+
+	//Productos por página
+	const CANTPORDHOME = 8;
+	const PROPORPAGINA = 4;
+	const PROCATEGORIA = 4;
+	const PROBUSCAR = 4;
+
+	//REDES SOCIALES
+	const FACEBOOK = "https://www.facebook.com";
+	const INSTAGRAM = "https://www.instagram.com";
+ ?>
